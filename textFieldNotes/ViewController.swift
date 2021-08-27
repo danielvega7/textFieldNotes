fb477a4f3c92f15def7d9f0072f9e87240802a95
Daniel Vega <daniel.vega23@d155.org>
2021-08-27 08:06:51 -0500

    text field notes. Finishing at home

--- textFieldNotes/ViewController.swift
+++ textFieldNotes/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textFieldOutlet: UITextField!
+    
+    @IBOutlet weak var nameResultLabelOutlet: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clickButtonAction(_ sender: UIButton) {
+        var userName = textFieldOutlet.text
+        nameResultLabelOutlet.text = userName
+    }
+    
 }
 
